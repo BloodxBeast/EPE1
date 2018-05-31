1e3134e9f209208ceb5f8261b403386e2487c7f1
blackness25 <black_alonsotek@hotmail.com>
2018-05-31 02:56:53 -0400

    coneccion con fire base en sugerencias implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,17 @@ import { QuienesSomosPage } from '../pages/quienes-somos/quienes-somos';
 import { AdultosPage } from '../pages/adultos/adultos';
 import { SugerenciasPage } from '../pages/sugerencias/sugerencias';
 import { RecreacionPage } from '../pages/recreacion/recreacion';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCUgtslUd0UdlzWY6NWkIW-X_-C_pTdc44",
+  authDomain: "appasperger.firebaseapp.com",
+  databaseURL: "https://appasperger.firebaseio.com",
+  projectId: "appasperger",
+  storageBucket: "appasperger.appspot.com",
+  messagingSenderId: "478828909687"
+};
+
 
 @Component({
   templateUrl: 'app.html'
@@ -20,7 +31,7 @@ export class MyApp {
   @ViewChild('NAV') nav: Nav;
   public rootPage: any;
   public pages: Array<{ titulo: string, component: any, icon: string }>;
-
+  
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
 
     this.rootPage = HomePage;
@@ -29,13 +40,14 @@ export class MyApp {
       { titulo: 'Escuelas', component: EscuelasPage, icon: 'school' },
       { titulo: 'Informacion', component: InformacionPage, icon: 'information-circle' },
       { titulo: 'Sintomas', component: SintomasPage, icon: 'heart' },
-      { titulo: 'Recreacion', component: RecreacionPage, icon: 'color-palette'},     
+      { titulo: 'Recreacion', component: RecreacionPage, icon: 'color-palette' },
       { titulo: 'En adultos', component: AdultosPage, icon: 'contact' },
       { titulo: 'Sugerencia', component: SugerenciasPage, icon: 'create' },
       { titulo: 'Acerca de', component: QuienesSomosPage, icon: 'star-half' },
       { titulo: 'Contacto', component: ContactoPage, icon: 'mail' }
-      
+
     ];
+
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -43,10 +55,13 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 
   goToPage(page) {
     this.nav.setRoot(page);
+    
   }
+  
 }
 
--- src/pages/sugerencias/sugerencias.ts
+++ src/pages/sugerencias/sugerencias.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import * as firebase from'firebase';
 /**
  * Generated class for the SugerenciasPage page.
  *
@@ -14,12 +14,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'sugerencias.html',
 })
 export class SugerenciasPage {
-
+  titulo: string = "";
+  nombre: string = "";
+  comentario: string = "";
+ 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SugerenciasPage');
   }
-
+  enviarSugerencia(){
+    var sugerenciaRef = firebase.database().ref().child("sugerencia");
+    sugerenciaRef.push({ titulo: this.titulo, nombre: this.nombre, comentario: this.comentario });
+    this.comentario="";
+    this.titulo="";
+    this.nombre="";
+  }
 }
